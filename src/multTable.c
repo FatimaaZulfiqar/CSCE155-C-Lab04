c078c574bacd09cd4c38b42df1db200ddccbec3f
Fatima Zulfiqar <zulfiqarfatima022@gmail.com>
2023-10-22 13:41:29 +0500

    lab 4

--- src/multTable.c
+++ src/multTable.c
@@ -20,8 +20,37 @@ int main(int argc, char **argv) {
     printf("empty table!\n");
     exit(0);
   }
+else{
 
-  //TODO: place your code here
+  printf("\nMULTIPLICATION TABLE\n\n\n");
+
+  printf("\n\n--");
+  
+  int m;
+  int x=1;
+  int y=1;
+
+  for(int r=1;r<=n;r++){
+    printf("%d\t",r);
+  }
+
+    printf("\n\n");
+
+    for(int i=1, i<=n; i++)
+    {
+      printf("%d",x);
+      x++;
+      for(int j=1, j<=n; j++)
+      {
+        m=i*j;
+        printf("%d\t",m);
+
+      }
+      printf("\n\n");
+
+
+    }
+}
 
   return 0;
 }
