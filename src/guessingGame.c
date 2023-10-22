d1897e3aec071b076deffb97fa957c3912df47b0
Fatima Zulfiqar <zulfiqarfatima022@gmail.com>
2023-10-22 13:44:21 +0500

    lab 3

--- src/guessingGame.c
+++ src/guessingGame.c
@@ -23,15 +23,22 @@ int main(int argc, char **argv) {
   // the user will try to guess what this number is
   int number = (rand() % n) + 1;
 
-  // initialize the user's "guess" to be an invalid value
-  int guess = -10;
-  int numGuesses = 0;
+  printf("Guess a number!\n\n");
+  int guess;
+  while(1){
 
-  printf("Guess-A-Number Game!\n");
-  printf("Enter a number between 1 and %d\n", n);
+    printf("\n\nEnter a number between 1 and %d :",n);
 
-  //TODO: place your code here
+    scanf("%d,&guess");
 
-  printf("Congratulations, you found it!  Number of guesses: %d\n", numGuesses);
+    if(guess==number){
+      printf("\n\nCongratulations, you did it! Number of gusses; %d\n",number);
+      break;
+    }
+    
+    else{
+      printf("\nINCORRECT! try again");
+    }
+      }
   return 0;
 }
